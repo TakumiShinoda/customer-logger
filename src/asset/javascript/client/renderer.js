94932b98c173cc2e211b7ca3536b42a9150b330f
TakumiShinoda <takumishinoda@TakumiShinodanoMacBook-Pro.local>
2017-12-02 09:52:07 +0900

    add IPAdress status in client/index

--- src/asset/javascript/client/renderer.js
+++ src/asset/javascript/client/renderer.js
@@ -1,3 +1,22 @@
+$(document).ready(() => {
+  function getIP(){
+    const os = require('os');
+    const interfaces = os.networkInterfaces();
+    var addresses = [];
+
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        }
+    }
+    return addresses[0];
+  }
+  $('p.ipAdress')[0].textContent = getIP();
+});
+
 ipcRenderer.on('addCustomer', (ev, id) => {
   $('ul#content').append('<li id=list class='+ id +'><span class='+ id +'>'+ id +'</span> - 02:00</li>')
 });
